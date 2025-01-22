66ecbcbecd5bfe2163f3b4e3364787eed9291f7d
Lauri Koppström <lauri.koppstrom@tuni.fi>
2025-01-22 12:57:32 +0200

    Fibinakki

--- koodaus-harkka/NewScript.cs
+++ koodaus-harkka/NewScript.cs
@@ -3,6 +3,7 @@ using System;
 
 public partial class NewScript : Node
 {
+    int a = 0, b = 1, temp;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -12,5 +13,10 @@ public partial class NewScript : Node
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		GD.Print(a);
+
+        	temp = a;
+      	    a = b;
+       	   	b = temp + b;
 	}
 }

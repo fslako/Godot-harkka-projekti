bcf60ca2baa9076d0f884ae8543132dffffbc199
Lauri Koppström <lauri.koppstrom@tuni.fi>
2025-01-24 13:54:53 +0200

    yesk

--- koodaus-harkka/ProtoMover.cs
+++ koodaus-harkka/ProtoMover.cs
@@ -1,26 +1,15 @@
 using Godot;
 using System;
 
-public partial class NewScript : Node
+public partial class ProtoMover : Node
 {
-    int a = 0, b = 1, temp, c = 1;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		GD.Print("Hello World!");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if (a < 1600)
-		{
-			GD.Print($"Frame {c}: {a}");
-        	temp = a;
-      	    a = b;
-       	   	b = temp + b;
-			c += 1;
-		}
-
 	}
 }

d70582d77f6e2381dfde7d0536fc5735e2248b65
Lauri Koppström <lauri.koppstrom@tuni.fi>
2025-01-24 14:22:02 +0200

    jäbä tippuu

--- koodaus-harkka/ProtoMover.cs
+++ koodaus-harkka/ProtoMover.cs
@@ -1,15 +1,24 @@
 using Godot;
 using System;
 
-public partial class ProtoMover : Node
+namespace Snakegame
+{
+public partial class ProtoMover : Node2D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		GD.Print($"Lokaali sijainti: {Position}");
+		GD.Print($"Globaali sijainti: {GlobalPosition}");
+		GlobalPosition = new Vector2(100, 100);
+		GD.Print($"Lokaali sijainti: {Position}");
+		GD.Print($"Globaali sijainti: {GlobalPosition}");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		GlobalPosition += Vector2.Down;
 	}
+}
 }

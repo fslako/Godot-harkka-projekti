445c1137e40311b6ec237ae80891c8841026406f
Lauri Koppström <lauri.koppstrom@tuni.fi>
2025-01-30 15:53:32 +0200

    jäbä liikkuu haluttuun suuntaan

--- koodaus-harkka/ProtoMover.cs
+++ koodaus-harkka/ProtoMover.cs
@@ -5,6 +5,9 @@ namespace Snakegame
 {
 public partial class ProtoMover : Node2D
 {
+	[Export] private float _x;
+	[Export] private float _y;
+	[Export] private float _speed;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -18,7 +21,9 @@ public partial class ProtoMover : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		GlobalPosition += Vector2.Down;
+		Vector2 icon = new Vector2(_x, _y);
+		GlobalPosition += icon * (float)delta * _speed;
+
 	}
 }
 }
